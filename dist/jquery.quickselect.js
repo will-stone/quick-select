bdddf442473f64398b737b0d25b814acfa4c5279
Will Stone <will@mac.home>
2015-10-10 15:28:14 +0100

    Bug fix
    Hidden class fix.

--- dist/jquery.quickselect.js
+++ dist/jquery.quickselect.js
@@ -69,7 +69,7 @@
 			if(breakOutAll) {
 				// Hide select overflow as all elements have been broken out. Can't use display none as
 				// the value will not be submitted.
-				$(el).addClass(namespace + 'hidden');
+				$(el).addClass(namespace + '__hidden');
 			} else {
 				// move select box inside wrapper
 				$(el)
